da0948f2cfe4c559b4d435b5d4f93135b27632a7
zhaolinxu <zhaolinxu@sina.cn>
2021-06-04 10:26:34 +0800

    加入汉化

---
+++ chs.js
@@ -0,0 +1,178 @@
+/*
+
+ @name    : 锅巴汉化 - Web汉化插件
+ @author  : 麦子、JAR、小蓝、好阳光的小锅巴
+ @version : V0.6.1 - 2019-07-09
+ @website : http://www.g8hh.com
+
+*/
+
+//1.汉化杂项
+var cnItems = {
+    _OTHER_: [],
+
+    //未分类：
+    'Save': '保存',
+    'Export': '导出',
+    'Import': '导入',
+    'Settings': '设置',
+    'Achievements': '成就',
+    'Statistics': '统计',
+    'Changelog': '更新日志',
+    'Hotkeys': '快捷键',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+    '': '',
+
+    //树游戏
+    'Loading...': '加载中...',
+    'ALWAYS': '一直',
+    'HARD RESET': '硬重置',
+    'Export to clipboard': '导出到剪切板',
+    'INCOMPLETE': '不完整',
+    'HIDDEN': '隐藏',
+    'AUTOMATION': '自动',
+    'NEVER': '从不',
+    'ON': '打开',
+    'OFF': '关闭',
+    'SHOWN': '显示',
+    'Play Again': '再次游戏',
+    'Keep Going': '继续',
+    'The Modding Tree Discord': '模型树Discord',
+    'You have': '你有',
+    'It took you {{formatTime(player.timePlayed)}} to beat the game.': '花费了 {{formatTime(player.timePlayed)}} 时间去通关游戏.',
+    'Congratulations! You have reached the end and beaten this game, but for now...': '恭喜你！ 您已经结束并通关了本游戏，但就目前而言...',
+    'Main Prestige Tree server': '主声望树服务器',
+    'Reach {{formatWhole(ENDGAME)}} to beat the game!': '达到 {{formatWhole(ENDGAME)}} 去通关游戏!',
+    '': '',
+    '': '',
+
+}
+
+
+//需处理的前缀
+var cnPrefix = {
+    "(-": "(-",
+    "(+": "(+",
+    "(": "(",
+    "-": "-",
+    "+": "+",
+    " ": " ",
+    ": ": "： ",
+    "\n": "",
+    "                   ": "",
+    "                  ": "",
+    "                 ": "",
+    "                ": "",
+    "               ": "",
+    "              ": "",
+    "             ": "",
+    "            ": "",
+    "           ": "",
+    "          ": "",
+    "         ": "",
+    "        ": "",
+    "       ": "",
+    "      ": "",
+    "     ": "",
+    "    ": "",
+    "   ": "",
+    "  ": "",
+    " ": "",
+    //树游戏
+    "Show Milestones: ": "显示里程碑：",
+    "Autosave: ": "自动保存: ",
+    "Offline Prod: ": "离线生产: ",
+    "Completed Challenges: ": "完成的挑战: ",
+    "High-Quality Tree: ": "高质量树贴图: ",
+    "Offline Time: ": "离线时间: ",
+    "Theme: ": "主题: ",
+    "": "",
+    "": "",
+    "": "",
+    "": "",
+    "": "",
+    "": "",
+}
+
+//需处理的后缀
+var cnPostfix = {
+    ":": "：",
+    "：": "：",
+    ": ": "： ",
+    "： ": "： ",
+    "/s)": "/s)",
+    "/s": "/s",
+    ")": ")",
+    "%": "%",
+    "                   ": "",
+    "                  ": "",
+    "                 ": "",
+    "                ": "",
+    "               ": "",
+    "              ": "",
+    "             ": "",
+    "            ": "",
+    "           ": "",
+    "          ": "",
+    "         ": "",
+    "        ": "",
+    "       ": "",
+    "      ": "",
+    "     ": "",
+    "    ": "",
+    "   ": "",
+    "  ": "",
+    " ": " ",
+    "\n": "",
+    "": "",
+    "": "",
+    "": "",
+    "": "",
+    "": "",
+    "": "",
+    "": "",
+    "": "",
+    "": "",
+    "": "",
+}
+
+//需排除的，正则匹配
+var cnExcludeWhole = [
+    /^(\d+)$/,
+    /^([\d\.]+)e(\d+)$/,
+    /^([\d\.]+)$/,
+];
+var cnExcludePostfix = [
+]
+
+//正则替换，带数字的固定格式句子
+//纯数字：(\d+)
+//逗号：([\d\.,]+)
+//小数点：([\d\.]+)
+//原样输出的字段：(.+)
+//换行加空格：\n(.+)
+var cnRegReplace = new Map([
+	[/^([\d\.]+)\/sec$/, '$1\/秒'],
+    [/^requires ([\d\.]+) more research points$/, '需要$1个研究点'],
+    [/^(\d+) Royal points$/, '$1 皇家点数'],
+    [/^Cost: (\d+) RP$/, '成本：$1 皇家点数'],
+    [/^Usages: (\d+)\/$/, '用途：$1\/'],
+    [/^workers: (\d+)\/$/, '工人：$1\/'],
+
+]);
